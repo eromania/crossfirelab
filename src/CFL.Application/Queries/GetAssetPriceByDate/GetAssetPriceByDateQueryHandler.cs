14946ed62f02f9a73817b1bc8756aaffd5620dd5
Erdem C <ciceke@itu.edu.tr>
2022-06-23 14:29:31 +0300

    GetAssetPriceByDateQueryHandler

--- src/CFL.Application/Queries/GetAssetPriceByDate/GetAssetPriceByDateQueryHandler.cs
+++ src/CFL.Application/Queries/GetAssetPriceByDate/GetAssetPriceByDateQueryHandler.cs
@@ -6,29 +6,32 @@ using Microsoft.EntityFrameworkCore;
 
 namespace CFL.Application.Queries.GetAssetPriceByDate;
 
-public class GetAssetPriceByDateQueryHandler : IRequestHandler<GetAssetPriceByDateQuery, List<GetAssetPriceByDateQueryViewModel>>
+public class
+    GetAssetPriceByDateQueryHandler : IRequestHandler<GetAssetPriceByDateQuery, List<GetAssetPriceByDateQueryViewModel>>
+{
+    private readonly IApplicationDbContext _context;
+    private readonly IMapper _mapper;
+
+    public GetAssetPriceByDateQueryHandler(
+        IApplicationDbContext context,
+        IMapper mapper)
     {
-        private readonly IApplicationDbContext _context;
-        private readonly IMapper _mapper;
-        public GetAssetPriceByDateQueryHandler(
-            IApplicationDbContext context,
-            IMapper mapper)
-        {
-            _context = context;
-            _mapper = mapper;
-        }
+        _context = context;
+        _mapper = mapper;
+    }
 
-        public async Task<List<GetAssetPriceByDateQueryViewModel>> Handle(GetAssetPriceByDateQuery request, CancellationToken 
+    public async Task<List<GetAssetPriceByDateQueryViewModel>> Handle(GetAssetPriceByDateQuery request,
+        CancellationToken
             cancellationToken)
-        {
-            var result = await _context.ExchangeRates
-                .Where(c => c.IsValid == 1
-                            && c.Time > DateTime.Now.AddDays(-request.Days))
-                .OrderBy(c => c.Time)
-                .AsNoTracking()
-                .ProjectTo<GetAssetPriceByDateQueryViewModel>(_mapper.ConfigurationProvider)
-                .ToListAsync(cancellationToken);
+    {
+        var result = await _context.ExchangeRates
+            .Where(c => c.IsValid == 1
+                        && c.Time > DateTime.Now.AddDays(-request.Days))
+            .OrderBy(c => c.Time)
+            .AsNoTracking()
+            .ProjectTo<GetAssetPriceByDateQueryViewModel>(_mapper.ConfigurationProvider)
+            .ToListAsync(cancellationToken);
 
-            return result;
-        }
+        return result;
+    }
 }
